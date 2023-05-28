846c498daad43b5d3f18eeeac974fb0c6173f6bd
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-28 18:09:33 +0200

    added new font called SF Pro in 3 diffrent variation locally

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab aperiam perferendis molestiae officia ratione a repellendus, nesciunt
+        itaque nisi fuga, sed quod laudantium veritatis dolorem rem. Adipisci autem dolor quasi?
+      </h1>
+    </div>
+  );
 }
 
 export default App;
