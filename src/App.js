ebf0187ecbd5ca157e55be88e78909cdb786bd21
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-28 22:14:21 +0200

    ready to start the project :)

--- src/App.js
+++ src/App.js
@@ -1,34 +1,7 @@
 import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum tempore necessitatibus fuga facere quasi pariatur repellat
-        repudiandae molestiae, temporibus aliquid iure officia explicabo maxime aliquam minima ea magni eius nesciunt! Odio, aperiam alias
-        animi exercitationem, iste asperiores iure velit tenetur consequuntur, magnam nobis qui illo quod vel. At veritatis deserunt autem
-        cumque. Numquam, fugiat voluptatem quibusdam ad fuga velit illo vel iste tempore, maxime ea sit aperiam magni dolore quas porro
-        odit, perspiciatis soluta impedit amet error ipsam necessitatibus ut accusantium? Inventore facere, eos illo non eum aspernatur ad
-        rem quae quas at, itaque nam voluptatibus nemo obcaecati fugiat dolorem magni blanditiis cumque. Rerum commodi asperiores cupiditate
-        nihil nobis temporibus recusandae, excepturi repudiandae cumque porro corrupti magnam eaque voluptatem itaque quam similique
-        tenetur. Maiores exercitationem iste a, nostrum cupiditate unde dolores ex laudantium facilis suscipit incidunt placeat consequatur
-        corporis architecto. Libero earum reiciendis expedita est voluptatibus vero dolores minima delectus exercitationem recusandae ipsum,
-        quo quisquam ipsa, velit qui! Ipsum quibusdam ratione beatae sit soluta rem deleniti a quod eos vitae. Deleniti, similique eaque?
-        Rerum recusandae temporibus laborum nulla dignissimos dolorem amet incidunt molestiae cumque reiciendis quaerat aperiam quas, facere
-        vel est pariatur ipsa odit fuga debitis! Dolores, quae autem?
-      </p>
-
-      <button>
-        Learn More<i class="ri-arrow-right-s-line"></i>
-      </button>
-
-      <h1>Pizza Land</h1>
-      <h2>Pizza Land</h2>
-      <h3>Pizza Land</h3>
-      <h4>Pizza Land</h4>
-      <h5>Pizza Land</h5>
-    </div>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
