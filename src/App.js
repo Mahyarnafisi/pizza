ace2cf77618d69a1ba2137f712f1047878a8fe8a
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-28 18:19:30 +0200

    initiated some diffult property in index.css

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab aperiam perferendis molestiae officia ratione a repellendus, nesciunt
-        itaque nisi fuga, sed quod laudantium veritatis dolorem rem. Adipisci autem dolor quasi?
-      </h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eum tempore necessitatibus fuga facere quasi pariatur repellat
+        repudiandae molestiae, temporibus aliquid iure officia explicabo maxime aliquam minima ea magni eius nesciunt! Odio, aperiam alias
+        animi exercitationem, iste asperiores iure velit tenetur consequuntur, magnam nobis qui illo quod vel. At veritatis deserunt autem
+        cumque. Numquam, fugiat voluptatem quibusdam ad fuga velit illo vel iste tempore, maxime ea sit aperiam magni dolore quas porro
+        odit, perspiciatis soluta impedit amet error ipsam necessitatibus ut accusantium? Inventore facere, eos illo non eum aspernatur ad
+        rem quae quas at, itaque nam voluptatibus nemo obcaecati fugiat dolorem magni blanditiis cumque. Rerum commodi asperiores cupiditate
+        nihil nobis temporibus recusandae, excepturi repudiandae cumque porro corrupti magnam eaque voluptatem itaque quam similique
+        tenetur. Maiores exercitationem iste a, nostrum cupiditate unde dolores ex laudantium facilis suscipit incidunt placeat consequatur
+        corporis architecto. Libero earum reiciendis expedita est voluptatibus vero dolores minima delectus exercitationem recusandae ipsum,
+        quo quisquam ipsa, velit qui! Ipsum quibusdam ratione beatae sit soluta rem deleniti a quod eos vitae. Deleniti, similique eaque?
+        Rerum recusandae temporibus laborum nulla dignissimos dolorem amet incidunt molestiae cumque reiciendis quaerat aperiam quas, facere
+        vel est pariatur ipsa odit fuga debitis! Dolores, quae autem?
+      </p>
+
+      <button>Start here here Here</button>
+
+      <h1>Pizza Land</h1>
+      <h2>Pizza Land</h2>
+      <h3>Pizza Land</h3>
+      <h4>Pizza Land</h4>
+      <h5>Pizza Land</h5>
     </div>
   );
 }
