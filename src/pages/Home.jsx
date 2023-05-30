3370a8acfbb12ccec97e34d79077663adbd4f332
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-05-30 21:49:31 +0200

    bug fix done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "../styles/home.css";
 
 function Home() {
-  return <div className="home"></div>;
+  return <div className="home">home</div>;
 }
 
 export default Home;
