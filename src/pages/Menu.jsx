812d4d7a629b30618f1d943a3a8dbfdb904e2ee6
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-03 22:39:35 +0200

    adding menu list and menu item , optimizing grid system in menu page

--- src/pages/Menu.jsx
+++ src/pages/Menu.jsx
@@ -4,7 +4,13 @@ import "../styles/menu.css";
 function Menu() {
   return (
     <div className="menu">
-      <div className="container"></div>
+      <div className="menu__header">
+        <h2 className="menu__title">Pizzas</h2>
+        <p className="menu__title-sub"> All fresh and tasty</p>
+      </div>
+      <div className="menu__list">
+        <div className="menu__item">card</div>
+      </div>
     </div>
   );
 }
