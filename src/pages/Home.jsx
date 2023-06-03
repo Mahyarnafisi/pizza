76fff99a72b6ee001d8be3b695efe7bcda9e60c4
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-03 14:29:48 +0200

    new backgroudn, add header with contents

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,30 @@
 import React from "react";
 import LogoVinatge from "../assets/logo-vintage.png";
+import Pizzaretro from "../assets/pizzaretro-bg-removed.png";
 import { Link } from "react-router-dom";
+import { BsChevronRight } from "react-icons/bs";
 import "../styles/home.css";
 
 function Home() {
   return (
     <div className="home">
-      <div className="home__left-box"></div>
+      <div className="home__left-box">
+        <div className="header">
+          <h1>Pizza</h1>
+          <h1>Best Italian pizza in town</h1>
+          <div className="left-box__header__rope">&nbsp;</div>
+
+          <Link to="/menu">
+            <button className="header__btn-menu">
+              See all menu <BsChevronRight className="btn-icon" />
+            </button>
+          </Link>
+        </div>
+        <div className="pizza-card"></div>
+      </div>
       <div className="home__right-box">
         <div className="right-box__card">
-          <img src={LogoVinatge} alt="retro picture" />
+          <img src={Pizzaretro} alt="retro picture" />
           <h2>
             The taste of <span>Italy</span>
           </h2>
