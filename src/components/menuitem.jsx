a13c55689abcc3906ca80f3511db796c7158676c
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-06 11:36:55 +0200

    bug fix change menuList database key name to id and fix th epronelm with key during mapping

--- src/components/menuitem.jsx
+++ src/components/menuitem.jsx
@@ -1,9 +1,19 @@
 import React from "react";
 import "../styles/menuItem.css";
-function MenuItem({ name, info, review, price }) {
+function MenuItem({ name, info, review, price, id }) {
   return (
     <div className="menu__item">
-      <p>{name}</p>
+      <div className="item__number">{id}</div>
+      <div className="item__image"></div>
+      <div className="item__card">
+        <h2 className="item__header">{name}</h2>
+        <p className="item__info">{info}</p>
+        <p className="item__review">{review}</p>
+        <div className="item__price">
+          <div className="item__taste"></div>
+          <div className="price">{price}</div>
+        </div>
+      </div>
     </div>
   );
 }
--- src/helpers/menuList.js
+++ src/helpers/menuList.js
@@ -8,13 +8,13 @@ import CaliforniaPizza from "../assets/pizza-7.webp";
 import DetroitPizza from "../assets/pizza-8.webp";
 
 const menuList = [
-  { key: 1, name: "Neapolitan Pizza", info: "affordable and readily available through numerous street vendors.", review: 92, price: 21.9, image: { NeapolitanPizza } },
-  { key: 2, name: "Chicago Pizza", info: "with slices of mozzarella lining the dough followed by meat, vegetables", review: 142, price: 18.9, image: { ChicagoPizza } },
-  { key: 3, name: "New York-Style Pizza", info: "toppings for Chicago pizza are ground beef, sausage, pepperoni, onion, mushrooms, and green peppers", review: 67, price: 16.9, image: { NewYorkStylePizza } },
-  { key: 4, name: "Sicilian Pizza", info: "Adding oil to the pan also helps to make the base of the dough a bit crispier", review: 188, price: 16.9, image: { SicilianPizza } },
-  { key: 5, name: "Greek Pizza", info: "unique flavor has to do with the minerals present in New York’s tap water supply.", review: 244, price: 19.9, image: { GreekPizza } },
-  { key: 6, name: "California Pizza", info: "California Pizza pizza usually features tomato sauce and mozzarella cheese. ", review: 542, price: 14.9, image: { CaliforniaPizza } },
-  { key: 7, name: "Detroit Pizza", info: "This square-cut pizza is served with or without cheese", review: 230, price: 12.9, image: { DetroitPizza } },
-  { key: 8, name: "St. Louis Pizza", info: "The sauce typically has a tangy tomato paste with a strong oregano flavor", review: 76, price: 19.9, image: { StLouisPizza } },
+  { id: 1, name: "Neapolitan Pizza", info: "affordable and readily available through numerous street vendors.", review: 92, price: 21.9, image: { NeapolitanPizza } },
+  { id: 2, name: "Chicago Pizza", info: "with slices of mozzarella lining the dough followed by meat, vegetables", review: 142, price: 18.9, image: { ChicagoPizza } },
+  { id: 3, name: "New York-Style Pizza", info: "toppings for Chicago pizza are ground beef, sausage, pepperoni, onion, mushrooms, and green peppers", review: 67, price: 16.9, image: { NewYorkStylePizza } },
+  { id: 4, name: "Sicilian Pizza", info: "Adding oil to the pan also helps to make the base of the dough a bit crispier", review: 188, price: 16.9, image: { SicilianPizza } },
+  { id: 5, name: "Greek Pizza", info: "unique flavor has to do with the minerals present in New York’s tap water supply.", review: 244, price: 19.9, image: { GreekPizza } },
+  { id: 6, name: "California Pizza", info: "California Pizza pizza usually features tomato sauce and mozzarella cheese. ", review: 542, price: 14.9, image: { CaliforniaPizza } },
+  { id: 7, name: "Detroit Pizza", info: "This square-cut pizza is served with or without cheese", review: 230, price: 12.9, image: { DetroitPizza } },
+  { id: 8, name: "St. Louis Pizza", info: "The sauce typically has a tangy tomato paste with a strong oregano flavor", review: 76, price: 19.9, image: { StLouisPizza } },
 ];
 export default menuList;
