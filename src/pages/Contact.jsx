37eba09c027f4425ef3323c1686ec0639d9b5e67
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-07 15:20:30 +0200

    added contact page background webp

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,13 +1,7 @@
 import React from "react";
 import "../styles/contact.css";
 function Contact() {
-  return (
-    <div className="contact">
-      <div className="container">
-        <h1>Contact page</h1>
-      </div>
-    </div>
-  );
+  return <div className="contact">contact</div>;
 }
 
 export default Contact;
