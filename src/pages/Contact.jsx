4c4584b557e60a7e6b267803b19e970f1e33c241
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-07 18:33:47 +0200

    done with about page and form

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -4,7 +4,19 @@ function Contact() {
   return (
     <div className="contact">
       <div className="contact__bg">bg</div>
-      <div className="contact__form">form</div>
+      <div className="contact__form">
+        <h1>Contact </h1>
+        <form className="form" method="post">
+          <label htmlFor="inputName">Your Full name</label>
+          <input className="form__inputName" name="inputName" type="text" placeholder="Full name" required />
+          <label htmlFor="inputEmail">Email Address</label>
+          <input className="form__inputEmail" name="inputEmail" type="email" placeholder="Email address" required />
+          <textarea id="" cols="30" rows="10" placeholder="Write your massage" required></textarea>
+          <button type="submit" className="form__submit-btn">
+            Send Massage
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
