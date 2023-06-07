8b3aee8a312ed7866731b0086ad8fe0ccb1b3a8e
mahyarnafisi <mahyar.nafisi@gmail.com>
2023-06-07 15:34:12 +0200

    layout the contact page, add bg image, now is ready to add form conetnts

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 import "../styles/contact.css";
 function Contact() {
-  return <div className="contact">contact</div>;
+  return (
+    <div className="contact">
+      <div className="contact__bg">bg</div>
+      <div className="contact__form">form</div>
+    </div>
+  );
 }
 
 export default Contact;
